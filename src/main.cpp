7fbc4fd1404c41fc35586d740dbb370e320f4d70
Asher <asher.haun@outlook.com>
2024-09-01 21:11:56 -0600

    get rid of christmas colors

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include "core/log.h"
 
 void hello() {
-    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x330000FF, 1.0f, 0);
+    bgfx::setViewClear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030FF, 1.0f, 0);
     bgfx::dbgTextClear();
-    bgfx::dbgTextPrintf(3, 3, 0x2f, "Hello World!");
+    bgfx::dbgTextPrintf(3, 3, 0x0f, "Hello World!");
     bgfx::setDebug(BGFX_DEBUG_TEXT);
     bgfx::touch(0);
 }
